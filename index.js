fea2612b9323540c5e422ce41283c2660e28738c
Henry Escobar <henrye@gmail.com>
2022-06-21 02:00:33 -0700

    commit tree 365efed3faefe31a3afecffa12be2ffb4922a1a8

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+  let reversedIndex = word.length - 1;
+  for (let i = 0; i < word.length; i++){
+    let letter = word[reversedIndex];
+
+    if (letter !== word[i]){
+      return false;
+    }
+    reversedIndex--;
+  }
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
